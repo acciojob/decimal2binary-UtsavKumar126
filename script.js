aba23dc7511252173c2edb180a3a2aa8f370bb31
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-08-17 22:40:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function decimalToBinary(num) {
-  //Write you code here
-  
+  //Write you code here 
+	let ans=[];
+	while(num>=0||num==1){
+		ans.push(num%2);
+		num=Math.floor(num/2)
+	}
+  let ansnum=0;
+  for(i=ans.length-1;i>=0;i--){
+	  ansnum=ansnum*10+ans[i];
+  }	
+
+return num;	
 }
 
 window.decimalToBinary = decimalToBinary;
